da905b18b7e4417003e1b36a40886481f20a0948
Emilien Chapon <emilien.chapon@cern.ch>
2015-09-07 17:16:23 +0200

    Add some flexibility to the plotting macro

--- HiAnalysis/HiTagAndProbe/macros/TnPEffDraw.C
+++ HiAnalysis/HiTagAndProbe/macros/TnPEffDraw.C
@@ -1,43 +1,72 @@
 #include <iostream>
-#include <TSystem.h>
-#include <TTree.h>
-#include <TKey.h>
-#include <TH1.h>
-#include <TH2.h>
-#include <TPave.h>
-#include <TText.h>
+#include "TSystem.h"
+#include "TTree.h"
+#include "TKey.h"
+#include "TH1.h"
+#include "TH2.h"
+#include "TPave.h"
+#include "TText.h"
 #include <sstream>
 #include <string.h>
 
-#include <TROOT.h>
-#include <TFile.h>
-#include <TGraphAsymmErrors.h>
-#include <TH1.h>
-#include <TH2.h>
-#include <TCanvas.h>
-#include <TLegend.h>
+#include "TROOT.h"
+#include "TFile.h"
+#include "TGraphAsymmErrors.h"
+#include "TH1.h"
+#include "TH2.h"
+#include "TCanvas.h"
+#include "TLegend.h"
 
-#include <RooFit.h>
-#include <RooRealVar.h>
-#include <RooDataSet.h>
-#include <RooArgSet.h>
+#include "RooFit.h"
+#include "RooRealVar.h"
+#include "RooDataSet.h"
+#include "RooArgSet.h"
 
-#include <TStyle.h>
-#include <TLatex.h>
-#include <TDirectory.h>
-#include <TCollection.h>
-#include <TPostScript.h>
+#include "TStyle.h"
+#include "TLatex.h"
+#include "TDirectory.h"
+#include "TCollection.h"
+#include "TPostScript.h"
 
 
 using namespace RooFit;
 using namespace std;
 
+const int nCentBins = 7;
+const double tx[nCentBins]={381.23,330.35,261.37,158.7,86.3,53.5,16.2};
+TString cutTag("MuonIDTrg");
+TString cutLegend("Muon ID");
+TString etaTag("PassingGlb_eta");
+TString ptTag("PassingGlb_pt");
+TString allTag("PassingGlb_1bin");
+const char* fCentMCNames[nCentBins] = 
+{
+   "MuIdTrg_cent_20150907/res_Cent0005_/data_MuIdTrg_Cent0005_.root",
+   "MuIdTrg_cent_20150907/res_Cent0510_/data_MuIdTrg_Cent0510_.root",
+   "MuIdTrg_cent_20150907/res_Cent1020_/data_MuIdTrg_Cent1020_.root",
+   "MuIdTrg_cent_20150907/res_Cent2040_/data_MuIdTrg_Cent2040_.root",
+   "MuIdTrg_cent_20150907/res_Cent4050_/data_MuIdTrg_Cent4050_.root",
+   "MuIdTrg_cent_20150907/res_Cent5060_/data_MuIdTrg_Cent5060_.root",
+   "MuIdTrg_cent_20150907/res_Cent60100_/data_MuIdTrg_Cent60100_.root"
+};
+const char* fCentDataNames[nCentBins] = 
+{
+   "MuIdTrg_cent_20150907/res_Cent0005_/data_MuIdTrg_Cent0005_.root",
+   "MuIdTrg_cent_20150907/res_Cent0510_/data_MuIdTrg_Cent0510_.root",
+   "MuIdTrg_cent_20150907/res_Cent1020_/data_MuIdTrg_Cent1020_.root",
+   "MuIdTrg_cent_20150907/res_Cent2040_/data_MuIdTrg_Cent2040_.root",
+   "MuIdTrg_cent_20150907/res_Cent4050_/data_MuIdTrg_Cent4050_.root",
+   "MuIdTrg_cent_20150907/res_Cent5060_/data_MuIdTrg_Cent5060_.root",
+   "MuIdTrg_cent_20150907/res_Cent60100_/data_MuIdTrg_Cent60100_.root"
+};
+const char* fPtDataName = "MuIdTrg_3etabins_20150907/tnp_Ana_RD_PbPb_MuonIDTrg_AllMB.root";
+const char* fEtaDataName = "MuIdTrg_3etabins_20150907/tnp_Ana_RD_PbPb_MuonIDTrg_AllMB.root";
 
 // Function Define
 TH2F *plotEff2D(RooDataSet *a, TString b);
 TGraphAsymmErrors *plotEffPt(RooDataSet *a, int aa);
 TGraphAsymmErrors *plotEffEta(RooDataSet *a, int aa);
-TGraphAsymmErrors *plotEffCent(RooDataSet *a1, RooDataSet *a2,RooDataSet *a3,RooDataSet *a4,int aa);
+TGraphAsymmErrors *plotEffCent(RooDataSet **a1,int aa);
 void formatTH1F(TH1* a, int b, int c, int d);
 void formatTGraph(TGraph* a, int b, int c, int d);
 void formatTLeg(TLegend* a);
@@ -46,7 +75,7 @@ void CalEffErr(TGraph *a, double *b);
 // From here you need to set up your environments.
 void TnPEffDraw() {
 
-  gROOT->Macro("~/logon.C");
+  // gROOT->Macro("~/logon.C");
   gROOT->SetStyle("Plain");
   gStyle->SetOptStat(0);
   gStyle->SetTitle(0);
@@ -56,51 +85,50 @@ void TnPEffDraw() {
 
 
   //centrallity root files
-  TFile *f1 = new TFile("Ana_Trk_v2_pbpb/tnp_Ana_PbPb_MuonTrk_MC_0010_20150514.root");
-  TFile *f2 = new TFile("Ana_Trk_v2_pbpb/tnp_Ana_PbPb_MuonTrk_MC_1020_20150514.root");
-  TFile *f3 = new TFile("Ana_Trk_v2_pbpb/tnp_Ana_PbPb_MuonTrk_MC_2050_20150514.root");
-  TFile *f4 = new TFile("Ana_Trk_v2_pbpb/tnp_Ana_PbPb_MuonTrk_MC_50100_20150514.root");
-  TFile *f5 = new TFile("Ana_Trk_v2_pbpb/tnp_Ana_PbPb_MuonTrk_RD_0010_20150514.root");
-  TFile *f6 = new TFile("Ana_Trk_v2_pbpb/tnp_Ana_PbPb_MuonTrk_RD_1020_20150514.root");
-  TFile *f7 = new TFile("Ana_Trk_v2_pbpb/tnp_Ana_PbPb_MuonTrk_RD_2050_20150514.root");
-  TFile *f8 = new TFile("Ana_Trk_v2_pbpb/tnp_Ana_PbPb_MuonTrk_RD_50100_20150514.root");
+  TFile* fCentMC[nCentBins];
+  TFile* fCentData[nCentBins];
+
+  for (int i=0; i<nCentBins; i++)
+  {
+     fCentMC[i] = new TFile(fCentMCNames[i]);
+     fCentData[i] = new TFile(fCentDataNames[i]);
+  }
   
   //pt and eta root files as well as single bin for integrated efficiency
-  TFile *f9 = new TFile("Ana_Trk_v2_pbpb/tnp_Ana_PbPb_MuonTrk_MC_OldBinNormBkg_20150514.root");
-  TFile *f10 = new TFile("Ana_Trk_v2_pbpb/tnp_Ana_PbPb_MuonTrk_RD_All_OldBinNormalBkg_20150514.root");
+  TFile *f9 = new TFile(fEtaDataName);
+  TFile *f10 = new TFile(fEtaDataName);
 
   TCanvas *c1 = new TCanvas("c1","",700,600);
 
-  RooDataSet *daPtData0 = (RooDataSet*)f9->Get("MuonTrk/PassingGlb_pt/fit_eff"); 
-  RooDataSet *daPtData1 = (RooDataSet*)f10->Get("MuonTrk/PassingGlb_pt/fit_eff"); 
+  RooDataSet *daPtData0 = (RooDataSet*)f9->Get(cutTag + "/" + ptTag + "/fit_eff"); 
+  RooDataSet *daPtData1 = (RooDataSet*)f10->Get(cutTag + "/" + ptTag + "/fit_eff"); 
 
   TGraphAsymmErrors *ComPt0 = plotEffPt(daPtData0, 1);
   TGraphAsymmErrors *ComPt1 = plotEffPt(daPtData1, 1);
 
-  RooDataSet *daEtaData0 = (RooDataSet*)f9->Get("MuonTrk/PassingGlb_eta/fit_eff"); 
-  RooDataSet *daEtaData1 = (RooDataSet*)f10->Get("MuonTrk/PassingGlb_eta/fit_eff"); 
+  RooDataSet *daEtaData0 = (RooDataSet*)f9->Get(cutTag + "/" + etaTag + "/fit_eff"); 
+  RooDataSet *daEtaData1 = (RooDataSet*)f10->Get(cutTag + "/" + etaTag + "/fit_eff"); 
 
  
   TGraphAsymmErrors *ComEta0 = plotEffEta(daEtaData0, 1);
   TGraphAsymmErrors *ComEta1 = plotEffEta(daEtaData1, 1);
 
-  RooDataSet *daCentMC1Bin0 = (RooDataSet*)f1->Get("MuonTrk/isGlb_1bin/fit_eff"); 
-  RooDataSet *daCentMC1Bin1 = (RooDataSet*)f2->Get("MuonTrk/isGlb_1bin/fit_eff"); 
-  RooDataSet *daCentMC1Bin2 = (RooDataSet*)f3->Get("MuonTrk/isGlb_1bin/fit_eff"); 
-  RooDataSet *daCentMC1Bin3 = (RooDataSet*)f4->Get("MuonTrk/isGlb_1bin/fit_eff"); 
-  RooDataSet *daCentData1Bin0 = (RooDataSet*)f5->Get("MuonTrk/isGlb_1bin/fit_eff"); 
-  RooDataSet *daCentData1Bin1 = (RooDataSet*)f6->Get("MuonTrk/isGlb_1bin/fit_eff"); 
-  RooDataSet *daCentData1Bin2 = (RooDataSet*)f7->Get("MuonTrk/isGlb_1bin/fit_eff"); 
-  RooDataSet *daCentData1Bin3 = (RooDataSet*)f8->Get("MuonTrk/isGlb_1bin/fit_eff"); 
+  RooDataSet *daCentMC1Bin[nCentBins];
+  RooDataSet *daCentData1Bin[nCentBins];
+  for (int i=0; i<nCentBins; i++)
+  {
+     daCentMC1Bin[i] = (RooDataSet*) fCentMC[i]->Get(cutTag + "/" + allTag + "/fit_eff");
+     daCentData1Bin[i] = (RooDataSet*) fCentData[i]->Get(cutTag + "/" + allTag + "/fit_eff");
+  }
 
-  RooDataSet *daPtMC1Bin0 = (RooDataSet*)f9->Get("MuonTrk/isGlb_1bin/fit_eff"); 
-  RooDataSet *daPtData1Bin0 = (RooDataSet*)f10->Get("MuonTrk/isGlb_1bin_High/fit_eff"); 
+  RooDataSet *daPtMC1Bin0 = (RooDataSet*)f9->Get(cutTag + "/" + allTag + "/fit_eff"); 
+  RooDataSet *daPtData1Bin0 = (RooDataSet*)f10->Get(cutTag + "/" + allTag + "/fit_eff"); 
 
   TGraphAsymmErrors *Com0Pt0 = plotEffPt(daPtMC1Bin0,0);
   TGraphAsymmErrors *Com0Pt1 = plotEffPt(daPtData1Bin0,0);
 
-  TGraphAsymmErrors *effCentMC = plotEffCent(daCentMC1Bin0,daCentMC1Bin1,daCentMC1Bin2,daCentMC1Bin3, 1);
-  TGraphAsymmErrors *effCentData = plotEffCent(daCentData1Bin0,daCentData1Bin1,daCentData1Bin2,daCentData1Bin3, 1);
+  TGraphAsymmErrors *effCentMC = plotEffCent(daCentMC1Bin, 1);
+  TGraphAsymmErrors *effCentData = plotEffCent(daCentData1Bin, 1);
 
   effCentMC->SetMarkerStyle(20);
   effCentMC->SetMarkerSize(1.4);
@@ -139,7 +167,7 @@ void TnPEffDraw() {
 
 
 
-  TH1F *hPad = new TH1F("hPad",";p^{#mu}_{T} [GeV/c];Single #mu Efficiency",5,3,20);
+  TH1F *hPad = new TH1F("hPad",";p^{#mu}_{T} [GeV/c];Single #mu Efficiency",5,0,30);
   TH1F *hPad1 = new TH1F("hPad1",";#eta^{#mu} ;Single #mu Efficiency",5,-2.4,2.4);
   TH1F *hPad2 = new TH1F("hPad2",";N_{part} ;Single #mu Efficiency",5,0,400);
   hPad->GetXaxis()->CenterTitle();
@@ -165,9 +193,9 @@ void TnPEffDraw() {
   hPad2->GetYaxis()->SetTitleOffset(1.);
 
 
-  hPad->GetYaxis()->SetRangeUser(0.4,1.05);
-  hPad1->GetYaxis()->SetRangeUser(0.4,1.05);
-  hPad2->GetYaxis()->SetRangeUser(0.4,1.05);
+  hPad->GetYaxis()->SetRangeUser(0.,1.05);
+  hPad1->GetYaxis()->SetRangeUser(0.,1.05);
+  hPad2->GetYaxis()->SetRangeUser(0.,1.05);
 
   hPad->Draw();
 
@@ -180,7 +208,7 @@ void TnPEffDraw() {
   leg1->SetFillColor(0);
   leg1->SetBorderSize(0);
   leg1->SetTextSize(0.035);
-  leg1->SetHeader("Inner Tracking Efficiency (p^{#mu}_{T}>3.5)");
+  leg1->SetHeader(cutLegend + " Efficiency (p^{#mu}_{T}>3.5)");
   sprintf(legs,"MC PYTHIA+EvtGen: %.4f^{ + %.3f}_{ - %.3f}", Trk0[0], Trk0[1], Trk0[2]);
   leg1->AddEntry(ComPt0,legs,"pl");
   sprintf(legs,"Data: %.4f^{ + %.3f}_{ - %.3f}", Trk1[0], Trk1[1], Trk1[2]);
@@ -195,8 +223,8 @@ void TnPEffDraw() {
   //lt1->DrawLatex(0.43,0.54,"pp  #sqrt{s} = 2.76 TeV");
   lt1->DrawLatex(0.43,0.54,"PbPb  #sqrt{s_{NN}} = 2.76 TeV");
 
-  c1->SaveAs("TrkEff_PbPb_RD_MC_PT.png");
-  c1->SaveAs("TrkEff_PbPb_RD_MC_PT.pdf");
+  c1->SaveAs(cutTag + "Eff_PbPb_RD_MC_PT.png");
+  c1->SaveAs(cutTag + "Eff_PbPb_RD_MC_PT.pdf");
 
    //---------- This is for eta dependence
   hPad1->Draw();
@@ -211,8 +239,8 @@ void TnPEffDraw() {
   //lt1->DrawLatex(0.43,0.54,"pp  #sqrt{s} = 2.76 TeV");
   lt1->DrawLatex(0.43,0.54,"PbPb  #sqrt{s_{NN}} = 2.76 TeV");
 
-  c1->SaveAs("TrkEff_PbPb_RD_MC_Eta.png");
-  c1->SaveAs("TrkEff_PbPb_RD_MC_Eta.pdf");
+  c1->SaveAs(cutTag + "Eff_PbPb_RD_MC_Eta.png");
+  c1->SaveAs(cutTag + "Eff_PbPb_RD_MC_Eta.pdf");
   
   //-------- This is for cent dependence
   hPad2->Draw();
@@ -227,8 +255,8 @@ void TnPEffDraw() {
   //lt1->DrawLatex(0.43,0.54,"pp  #sqrt{s} = 2.76 TeV");
   lt1->DrawLatex(0.43,0.54,"PbPb  #sqrt{s_{NN}} = 2.76 TeV");
 
-  c1->SaveAs("TrkEff_PbPb_RD_MC_Cent.png");
-  c1->SaveAs("TrkEff_PbPb_RD_MC_Cent.pdf");
+  c1->SaveAs(cutTag + "Eff_PbPb_RD_MC_Cent.png");
+  c1->SaveAs(cutTag + "Eff_PbPb_RD_MC_Cent.pdf");
 
 
 }
@@ -361,8 +389,8 @@ TGraphAsymmErrors *plotEffPt(RooDataSet *a, int aa){
 
   const int nbins = xAx->getBinning().numBins();
 
-  double tx[nbins], txhi[nbins], txlo[nbins];
-  double ty[nbins], tyhi[nbins], tylo[nbins];
+  double *tx = new double[nbins], *txhi = new double[nbins], *txlo = new double[nbins];
+  double *ty = new double[nbins], *tyhi = new double[nbins], *tylo = new double[nbins];
 
   for (int i=0; i<nbins; i++) {
     a->get(i);
@@ -374,7 +402,7 @@ TGraphAsymmErrors *plotEffPt(RooDataSet *a, int aa){
     tylo[i] = fabs(eff->getErrorLo()); 
   }
 
-  cout<<"NBins : "<<nbins<<endl;
+  cout<<"NBins pt: "<<nbins<<endl;
 
   const double *x = tx;
   const double *xhi = txhi;
@@ -407,35 +435,25 @@ TGraphAsymmErrors *plotEffPt(RooDataSet *a, int aa){
 
 }
 
-TGraphAsymmErrors *plotEffCent(RooDataSet *a1, RooDataSet *a2,RooDataSet *a3,RooDataSet *a4,int aa){
-  const RooArgSet *set1 = a1->get();
-  RooRealVar *eff1 = (RooRealVar*)set1->find("efficiency");
-  const RooArgSet *set2 = a2->get();
-  RooRealVar *eff2 = (RooRealVar*)set2->find("efficiency");
-  const RooArgSet *set3 = a3->get();
-  RooRealVar *eff3 = (RooRealVar*)set3->find("efficiency");
-  const RooArgSet *set4 = a4->get();
-  RooRealVar *eff4 = (RooRealVar*)set4->find("efficiency");
+TGraphAsymmErrors *plotEffCent(RooDataSet **a1,int aa){
+  const RooArgSet *setn[nCentBins];
+  RooRealVar *effn[nCentBins];
+  for (int i=0; i<nCentBins; i++)
+  {
+     setn[i] = a1[i]->get();
+     effn[i] = (RooRealVar*)setn[i]->find("efficiency");
+  }
 
-  const int nbins = 4;
+  const int nbins = nCentBins;
 
-  double tx[nbins]={355,261,134.26,22.06};
-  double ty[nbins], tyhi[nbins], tylo[nbins];
+  double ty[nCentBins], tyhi[nCentBins], tylo[nCentBins];
 
-    ty[0] = eff1->getVal();
-    tyhi[0] = fabs(eff1->getErrorHi());
-    tylo[0] = fabs(eff1->getErrorLo()); 
-    ty[1] = eff2->getVal();
-    tyhi[1] = fabs(eff2->getErrorHi());
-    tylo[1] = fabs(eff2->getErrorLo()); 
-    ty[2] = eff3->getVal();
-    tyhi[2] = fabs(eff3->getErrorHi());
-    tylo[2] = fabs(eff3->getErrorLo()); 
-    ty[3] = eff4->getVal();
-    tyhi[3] = fabs(eff4->getErrorHi());
-    tylo[3] = fabs(eff4->getErrorLo()); 
-
-  cout<<"NBins : "<<nbins<<endl;
+  for (int i=0; i<nCentBins; i++)
+  {
+     ty[i] = effn[i]->getVal();
+     tyhi[i] = fabs(effn[i]->getErrorHi());
+     tylo[i] = fabs(effn[i]->getErrorLo());
+  }
 
   const double *x = tx;
   const double *y = ty;
