1e2de3c1e35c112374596bab22dad16fafb04263
Andre Stahl <stahlleiton@gmail.com>
2017-05-12 14:52:06 +0200

    Add PA event selection

--- HeavyIonsAnalysis/Configuration/python/collisionEventSelection_cff.py
+++ HeavyIonsAnalysis/Configuration/python/collisionEventSelection_cff.py
@@ -4,27 +4,44 @@ import FWCore.ParameterSet.Config as cms
 from HeavyIonsAnalysis.Configuration.hfCoincFilter_cff import *
 
 # Selection of at least a two-track fitted vertex
-primaryVertexFilter = cms.EDFilter("VertexSelector",
+primaryVertexFilterHI = cms.EDFilter("VertexSelector",
     src = cms.InputTag("hiSelectedVertex"),
     cut = cms.string("!isFake && abs(z) <= 25 && position.Rho <= 2 && tracksSize >= 2"), 
     filter = cms.bool(True),   # otherwise it won't filter the events
 )
 
-# Cluster-shape filter re-run offline
-from RecoLocalTracker.SiPixelRecHits.SiPixelRecHits_cfi import *
-from HLTrigger.special.hltPixelClusterShapeFilter_cfi import *
-hltPixelClusterShapeFilter.inputTag = "siPixelRecHits"
-
-# Reject BSC beam halo L1 technical bits
-from L1TriggerConfig.L1GtConfigProducers.L1GtTriggerMaskTechTrigConfig_cff import *
-from HLTrigger.HLTfilters.hltLevel1GTSeed_cfi import hltLevel1GTSeed
-noBSChalo = hltLevel1GTSeed.clone(
-    L1TechTriggerSeeding = cms.bool(True),
-    L1SeedsLogicalExpression = cms.string('NOT (36 OR 37 OR 38 OR 39)')
+primaryVertexFilterPA = cms.EDFilter("VertexSelector",
+    src = cms.InputTag("offlinePrimaryVertices"),
+    cut = cms.string("!isFake && abs(z) <= 25 && position.Rho <= 2 && tracksSize >= 2"),
+    filter = cms.bool(True), # otherwise it won't filter the events
 )
 
-collisionEventSelection = cms.Sequence(noBSChalo *
-                                       hfCoincFilter3 *
-                                       primaryVertexFilter *
-                                       siPixelRecHits *
-                                       hltPixelClusterShapeFilter)
+
+# Cluster-shape filter re-run offline from ClusterCompatibility object for PbPb
+from HeavyIonsAnalysis.Configuration.HIClusterCompatibilityFilter_cfi import *
+
+#Reject beam scraping events standard pp and pA configuration
+NoScraping = cms.EDFilter("FilterOutScraping",
+ applyfilter = cms.untracked.bool(True),
+ debugOn = cms.untracked.bool(False),
+ numtrack = cms.untracked.uint32(10),
+ thresh = cms.untracked.double(0.25)
+)
+
+
+#Reject events with pile-up, optionally
+from HeavyIonsAnalysis.VertexAnalysis.PAPileUpVertexFilter_cff import *
+
+
+collisionEventSelection = cms.Sequence(hfCoincFilter3 *
+                                       primaryVertexFilterHI *
+                                       clusterCompatibilityFilter)
+
+collisionEventSelectionPA = cms.Sequence(hfCoincFilter *
+                                         primaryVertexFilterPA *
+                                         NoScraping
+                                         )
+
+collisionEventSelectionPA_rejectPU = cms.Sequence(collisionEventSelectionPA *
+                                                  pileupVertexFilterCutGplus)
+
