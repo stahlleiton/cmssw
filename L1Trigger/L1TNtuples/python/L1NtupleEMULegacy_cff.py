6cefd2db79c8627491be617d13d847638200f5a7
Andre Stahl <stahlleiton@gmail.com>
2017-06-20 17:44:09 +0200

    Include caloStage1FinalDigis in legacyMuonEmuTree L1NTuple

--- L1Trigger/L1TNtuples/python/L1NtupleEMULegacy_cff.py
+++ L1Trigger/L1TNtuples/python/L1NtupleEMULegacy_cff.py
@@ -4,7 +4,11 @@ from L1Trigger.L1TNtuples.l1UpgradeTree_cfi import *
 from L1Trigger.L1TNtuples.l1EventTree_cfi import *
 
 l1legacyMuonEmuTree = l1UpgradeTree.clone()
-l1legacyMuonEmuTree.muonToken = cms.untracked.InputTag("muonLegacyInStage2FormatDigis","imdMuonsLegacy")
+l1legacyMuonEmuTree.egToken   = cms.untracked.InputTag("caloStage1FinalDigis")
+l1legacyMuonEmuTree.jetToken  = cms.untracked.InputTag("caloStage1FinalDigis")
+l1legacyMuonEmuTree.sumToken  = cms.untracked.InputTag("caloStage1FinalDigis")
+l1legacyMuonEmuTree.tauToken  = cms.untracked.InputTag("caloStage1FinalDigis","rlxTaus")
+l1legacyMuonEmuTree.muonToken = cms.untracked.InputTag("simMuonLegacyInStage2FormatDigis","imdMuonsLegacy")
 
 L1NtupleEMULegacy = cms.Sequence(
   l1EventTree
