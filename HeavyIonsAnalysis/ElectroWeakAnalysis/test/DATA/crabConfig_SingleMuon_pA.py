67374418596381eb9863c6d8757617672b46676d
Andre Stahl <stahlleiton@gmail.com>
2017-08-13 23:42:44 +0200

    Updated the CRAB configuration file for DATA

--- HeavyIonsAnalysis/ElectroWeakAnalysis/test/DATA/crabConfig_SingleMuon_pA.py
+++ HeavyIonsAnalysis/ElectroWeakAnalysis/test/DATA/crabConfig_SingleMuon_pA.py
@@ -2,7 +2,7 @@ from CRABClient.UserUtilities import config, getUsernameFromSiteDB
 config = config()
 
 config.section_('General')
-config.General.requestName = 'PARun2016C-v1_Run_285479_286504_EWQANA_pA_20170717'
+config.General.requestName = 'PARun2016C-v1_Run_285479_286504_EWQANA_pA_20170813'
 config.General.workArea = 'crab_projects'
 config.General.transferOutputs = True
 config.General.transferLogs = False
@@ -15,7 +15,7 @@ config.JobType.maxMemoryMB = 2400
 config.section_('Data')
 config.Data.inputDataset ='/PASingleMuon/anstahll-HIEWQ_pPb8TeV_AOD_20170207-ea47420fa7d4dfad582bb824ce988a1b/USER'
 config.Data.inputDBS = 'phys03'
-config.Data.unitsPerJob = 10
+config.Data.unitsPerJob = 15
 config.Data.splitting = 'FileBased'
 config.Data.outLFNDirBase = '/store/user/%s/EWQAnalysis2017/Tree/%s' % (getUsernameFromSiteDB(), config.General.requestName)
 config.Data.publication = False
