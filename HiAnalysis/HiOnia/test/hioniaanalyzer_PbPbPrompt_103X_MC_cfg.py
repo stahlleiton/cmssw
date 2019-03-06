bf5a118d1c248d8f16df39475d54c95f95e94d72
batoul-d <batoul.j.diab@gmail.com>
2019-03-06 11:48:10 +0100

    the right centrality calibration for MC

--- HiAnalysis/HiOnia/test/hioniaanalyzer_PbPbPrompt_103X_MC_cfg.py
+++ HiAnalysis/HiOnia/test/hioniaanalyzer_PbPbPrompt_103X_MC_cfg.py
@@ -146,7 +146,7 @@ print('\n\033[31m~*~ USING CENTRALITY TABLE FOR PbPb 2018 ~*~\033[0m\n')
 process.GlobalTag.snapshotTime = cms.string("9999-12-31 23:59:59.000")
 process.GlobalTag.toGet.extend([
     cms.PSet(record = cms.string("HeavyIonRcd"),
-        tag = cms.string("CentralityTable_HFtowers200_DataPbPb_periHYDJETshape_run2v1031x02_offline"),
+        tag = cms.string("CentralityTable_HFtowers200_HydjetDrum5Ev8_v1030pre5x02_mc"),
         connect = cms.string("frontier://FrontierProd/CMS_CONDITIONS"),
         label = cms.untracked.string("HFtowers")
         ),
