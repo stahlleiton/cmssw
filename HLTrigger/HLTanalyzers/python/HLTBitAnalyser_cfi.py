bf72012ca1176f5bd595f36aa03c79dae2746eba
Andre Govinda Stahl Leiton <andre.govinda.stahl.leiton@cern.ch>
2020-03-25 15:53:42 +0100

    Updated thee HLTBitAnalyzer in 11_1_X

--- HLTrigger/HLTanalyzers/python/HLTBitAnalyser_cfi.py
+++ HLTrigger/HLTanalyzers/python/HLTBitAnalyser_cfi.py
@@ -1,15 +1,27 @@
 import FWCore.ParameterSet.Config as cms
 
 hltbitanalysis = cms.EDAnalyzer("HLTBitAnalyzer",
-    ### Trigger objects
+    ### General Settings
+    UseL1Stage2                     = cms.untracked.bool(True),
+    stageL1Trigger                  = cms.uint32(2),
+    getPrescales                    = cms.untracked.bool(False),
+    getL1InfoFromEventSetup         = cms.untracked.bool(False),
+
+    ### L1 Legacy and Stage 1 objects
     l1GctHFBitCounts                = cms.InputTag("hltGctDigis"),
     l1GctHFRingSums                 = cms.InputTag("hltGctDigis"),
-    l1GtObjectMapRecord             = cms.InputTag("hltL1GtObjectMap::HLT"),
-    l1results                       = cms.InputTag("hltGtDigis::HLT"),                            
-##    l1GtReadoutRecord               = cms.InputTag("hltGtDigis::HLT"),
-
+    l1GtReadoutRecord               = cms.InputTag("hltGtDigis::HLT"),
     l1extramc                       = cms.string('hltL1extraParticles'),
     l1extramu                       = cms.string('hltL1extraParticles'),
+
+    ### L1 Stage 2 objects
+    l1tAlgBlkInputTag               = cms.InputTag("hltGtStage2Digis"),  # Needed, fix bug of GlobalAlgBlk uninitialized token
+    l1tExtBlkInputTag               = cms.InputTag("hltGtStage2Digis"),
+    gObjectMapRecord                = cms.InputTag("hltGtStage2ObjectMap"),
+    gmtStage2Digis                  = cms.string("hltGtStage2Digis"),
+    caloStage2Digis                 = cms.string("hltGtStage2Digis"),
+
+    ### HLT
     hltresults                      = cms.InputTag("TriggerResults::HLT"),
     HLTProcessName                  = cms.string("HLT"),
 
@@ -19,17 +31,16 @@ hltbitanalysis = cms.EDAnalyzer("HLTBitAnalyzer",
 
     ### SIM objects
     simhits                         = cms.InputTag("g4SimHits"),
-                                
+
     ## reco vertices
-    OfflinePrimaryVertices0     = cms.InputTag('offlinePrimaryVertices'),
-                                
+    OfflinePrimaryVertices0         = cms.InputTag('offlinePrimaryVertices'),
+
     ### Run parameters
     RunParameters = cms.PSet(
         HistogramFile = cms.untracked.string('hltbitanalysis.root'),
         isData         = cms.untracked.bool(False),
         Monte          = cms.bool(True),
-        GenTracks      = cms.bool(True)
-
+        GenTracks      = cms.bool(False),
     )
-                                
+
 )
