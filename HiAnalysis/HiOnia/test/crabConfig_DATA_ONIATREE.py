2cb28d5b2178eeffcaee3cbbfb509339b2c75adc
Florian Damas <florian.damas@cern.ch>
2023-10-10 00:18:57 +0200

    fix crab config file for PromptReco data

--- HiAnalysis/HiOnia/test/crabConfig_DATA_ONIATREE.py
+++ HiAnalysis/HiOnia/test/crabConfig_DATA_ONIATREE.py
@@ -12,16 +12,18 @@ config.section_("JobType")
 config.JobType.pluginName = "Analysis"
 config.JobType.psetName = "hioniaanalyzer_PbPbPrompt_13_2_X_DATA_cfg.py"
 #config.JobType.maxMemoryMB = 2500         # request high memory machines.
-#config.JobType.numCores = 4
+config.JobType.numCores = 4
 config.JobType.allowUndistributedCMSSW = True #Problems with slc7
 #config.JobType.maxJobRuntimeMin = 1000 #2750    # request longer runtime, ~48 hours.
 
+config.JobType.inputFiles = ["CentralityTable_HFtowers200_DataPbPb_periHYDJETshape_run3v1302x04_offline_374289.db"]
+
 config.section_("Data")
-config.Data.inputDataset = '/HIPhysicsRawPrime0/HIRun2023A-PromptReco-v1/MINIAOD'
+config.Data.inputDataset = '/HIPhysicsRawPrime0/HIRun2023A-PromptReco-v2/MINIAOD'
 config.Data.inputDBS = 'global'
-#config.Data.unitsPerJob = 10
+config.Data.unitsPerJob = 50
 #config.Data.totalUnits = -1
-config.Data.splitting = "Automatic"
+config.Data.splitting = "LumiBased"
 config.Data.allowNonValidInputDataset = True
 
 config.Data.outLFNDirBase = '/store/user/fdamas/%s' % (config.General.requestName)
