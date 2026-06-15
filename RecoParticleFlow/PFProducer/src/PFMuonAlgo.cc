dc56acddab0c9e8efe036e39563010daa3d6db02
Andre Govinda Stahl Leiton <andre.govinda.stahl.leiton@cern.ch>
2026-06-15 05:48:49 +0200

    Integration of ME0 on PFMuon algorithm

--- RecoParticleFlow/PFProducer/src/PFMuonAlgo.cc
+++ RecoParticleFlow/PFProducer/src/PFMuonAlgo.cc
@@ -177,7 +177,9 @@ bool PFMuonAlgo::isTrackerTightMuon(const reco::MuonRef& muonRef) {
 
   unsigned nTrackerHits = track.hitPattern().numberOfValidTrackerHits();
 
-  if (nTrackerHits <= 12)
+  if (muonRef->isPhase2Muon() && std::abs(muonRef->eta()) > 2.3)
+    return nTrackerHits > 8;
+  else if (nTrackerHits <= 12)
     return false;
 
   bool isAllArbitrated = muon::isGoodMuon(*muonRef, muon::AllArbitrated);
